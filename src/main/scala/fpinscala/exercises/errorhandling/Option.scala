bde2030963987000e31740eb2039070065f36b0c
Zi Jing <bigzijing@gmail.com>
2026-07-27 23:08:22 +0800

    Finish Chapter 4: Options

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,15 +7,24 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = 
+    this match
+      case None => None
+      case Some(v) => Some(f(v))
+    
+  def getOrElse[B>:A](default: => B): B = 
+    this match
+      case None => default
+      case Some(v) => v    
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = 
+    map(f).getOrElse(None)
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = 
+    map(Some(_)).getOrElse(ob)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
-
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = 
+    flatMap(a => if f(a) then Some(a) else None)
 
 object Option:
 
@@ -36,10 +45,14 @@ object Option:
     if xs.isEmpty then None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = 
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = 
+    a.flatMap(a => b.map(b => f(a, b)))
 
-  def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](as: List[Option[A]]): Option[List[A]] = 
+    as.foldRight(Some(List.empty[A]))((next, acc) => map2(next, acc)(_ :: _))
 
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = 
+    as.foldRight(Some(List.empty[B]))((next, acc) => map2(f(next), acc)(_ :: _))  
